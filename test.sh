95a008346e0f4188d61e5c2e10cb3cdb33190936
Roberto Sero <>
2024-03-19 12:21:52 +0100

    chore: less tests for perf

--- test.sh
+++ test.sh
@@ -13,16 +13,16 @@ function t {
 
 docker-compose -f $C build -q
 
-t HTTP-POST sha256 exact 1
+# t HTTP-POST sha256 exact 1
 t HTTP-Redirect sha512 minimum 1
-t HTTP-POST sha256 maximum 1
+# t HTTP-POST sha256 maximum 1
 # t HTTP-Redirect sha512 better 1 # not working atm
-t HTTP-Redirect sha512 exact 2
+# t HTTP-Redirect sha512 exact 2
 t HTTP-POST sha256 minimum 2
-t HTTP-Redirect sha512 maximum 2
+# t HTTP-Redirect sha512 maximum 2
 # t HTTP-POST sha256 better 2 # not working atm
-t HTTP-Redirect sha512 exact 3
-t HTTP-POST sha256 minimum 3
+# t HTTP-Redirect sha512 exact 3
+# t HTTP-POST sha256 minimum 3
 t HTTP-Redirect sha512 maximum 3
 
 docker-compose -f $C down -v
