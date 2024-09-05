03b50c9ecd4e559c72552b752a5442a7d753818b
Roberto Sero <>
2024-09-05 12:25:01 +0200

    fix: github test workflow

--- test.sh
+++ test.sh
@@ -6,12 +6,11 @@ C="./test/docker-compose.yml"
 DEBUG=${DEBUG:-"WARNING"}
 
 function t {
-  docker-compose -f $C down -v
-  BINDING=$1 ALG=$2 RAC_COMPARISON=$3 AUTHN_CONTEXT=$4 DEBUG=$DEBUG docker-compose -f $C up --exit-code-from test --no-log-prefix test
-  # BINDING=$1 ALG=$2 RAC_COMPARISON=$3 AUTHN_CONTEXT=$4 DEBUG=$DEBUG docker-compose -f $C up
+  docker compose -f $C down -v
+  BINDING=$1 ALG=$2 RAC_COMPARISON=$3 AUTHN_CONTEXT=$4 DEBUG=$DEBUG docker compose -f $C up --exit-code-from test --no-log-prefix test
 }
 
-docker-compose -f $C build -q
+docker compose -f $C build -q
 
 # t HTTP-POST sha256 exact 1
 t HTTP-Redirect sha512 minimum 1
@@ -25,4 +24,4 @@ t HTTP-POST sha256 minimum 2
 # t HTTP-POST sha256 minimum 3
 t HTTP-Redirect sha512 maximum 3
 
-docker-compose -f $C down -v
+docker compose -f $C down -v
