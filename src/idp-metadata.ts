c10360b20f17a18ffca9cc5ae6e3188786b31d1d
Emanuele Mossa <emanuelemossa@MacBook-Pro-di-Emanuele.local>
2025-10-02 11:10:09 +0200

    ceck cert validity

--- src/idp-metadata.ts
+++ src/idp-metadata.ts
@@ -1,6 +1,34 @@
 import { IDPConfig } from './types';
 import { parseDom } from './xml';
 import { NS } from './const';
+import { X509Certificate } from 'crypto';
+
+export const getIdpCert = (idp: any) => {
+  const idpDescriptor = Array.from(
+    idp.getElementsByTagNameNS(NS.SAML_METADATA, 'IDPSSODescriptor'),
+  )[0];
+
+  let cert: string | null | undefined;
+  if (idpDescriptor) {
+    const keyDescriptors = Array.from(
+      idpDescriptor.getElementsByTagNameNS(NS.SAML_METADATA, 'KeyDescriptor'),
+    );
+    // Look for KeyDescriptor with use="signing" or without use attribute (means both)
+    const signingDescriptor = keyDescriptors.find(
+      (kd) => kd.getAttribute('use') === 'signing' || !kd.getAttribute('use'),
+    );
+    const certificates = signingDescriptor
+      ?.getElementsByTagNameNS(NS.SIG, 'X509Certificate')
+      .map((x) => x.textContent);
+
+    cert = certificates.find((certificate) => {
+      // Find a not expired X509Certificate
+      const { validTo } = new X509Certificate(certificate || '');
+      return new Date(validTo) > new Date();
+    });
+  }
+  return cert;
+};
 
 export const getIdentityProviders = (
   xml: string,
@@ -19,10 +47,12 @@ export const getIdentityProviders = (
       Array.from(idp.getElementsByTagNameNS(NS.SAML_METADATA, tag))
         .find((x) => x.getAttribute('Binding') === binding)
         ?.getAttribute('Location');
+
+    // Find a not expired X509Certificate in a keyDescriptor use="signing"
+
     return {
       entityId: idp.getAttribute('entityID'),
-      cert: idp.getElementsByTagNameNS(NS.SIG, 'X509Certificate').item(0)
-        ?.textContent,
+      cert: getIdpCert(idp),
       entryPoint: getLocation('SingleSignOnService'),
       logoutUrl: getLocation('SingleLogoutService'),
     };
