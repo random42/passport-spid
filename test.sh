e09f2155c3043d001f5e416a6bb9dcc3594f1e97
Roberto Sero <>
2025-10-12 18:18:45 +0200

    test: parallel testing

--- test.sh
+++ test.sh
@@ -1,36 +1,53 @@
 #!/usr/bin/env bash
-set -e
+set -e #-x
 
 C="./test/docker-compose.yml"
 
 DEBUG=${DEBUG:-"WARNING"}
 
-function down {
-  docker compose -f $C down -v
-}
+# Bring down any previous containers and build images
+docker compose -f $C build
 
-function t {
-  BINDING=$1 ALG=$2 RAC_COMPARISON=$3 AUTHN_CONTEXT=$4 DEBUG=$DEBUG docker compose -f $C up -d
-  docker compose -f $C logs -f test &
+# Define test cases as an array
+declare -a TESTS=(
+  "HTTP-Redirect sha512 minimum 1"
+  "HTTP-Redirect sha512 exact 2"
+  "HTTP-POST sha256 minimum 2"
+  "HTTP-Redirect sha512 maximum 3"
+)
+
+# Function to run a test in a subshell with a unique project name
+function run_test {
+  local TEST_ARGS=( $@ )
+  local BINDING=${TEST_ARGS[0]}
+  local ALG=${TEST_ARGS[1]}
+  local RAC_COMPARISON=${TEST_ARGS[2]}
+  local AUTHN_CONTEXT=${TEST_ARGS[3]}
+  local PROJECT="spidtest$RANDOM"
+  BINDING=$BINDING ALG=$ALG RAC_COMPARISON=$RAC_COMPARISON AUTHN_CONTEXT=$AUTHN_CONTEXT DEBUG=$DEBUG docker compose -f $C -p $PROJECT up -d > /dev/null 2>&1
+  docker compose -f $C -p $PROJECT logs -f test 2>/dev/null &
   LOGS_PID=$!
-  docker compose -f $C wait test
+  docker compose -f $C -p $PROJECT wait test 2>/dev/null
   TEST_EXIT_CODE=$?
   kill $LOGS_PID 2>/dev/null || true
-  down
+  docker compose -f $C -p $PROJECT down -v > /dev/null 2>&1
+  if [ $TEST_EXIT_CODE -ne 0 ]; then
+    echo "FAILED test: BINDING=$BINDING ALG=$ALG RAC_COMPARISON=$RAC_COMPARISON AUTHN_CONTEXT=$AUTHN_CONTEXT"
+  fi
   return $TEST_EXIT_CODE
 }
 
-down
-docker compose -f $C build
+# Run all tests in parallel
+PIDS=()
+for TEST in "${TESTS[@]}"; do
+  run_test $TEST &
+  PIDS+=( $! )
+done
 
-# t HTTP-POST sha256 exact 1
-t HTTP-Redirect sha512 minimum 1
-# t HTTP-POST sha256 maximum 1
-# t HTTP-Redirect sha512 better 1 # not working atm
-# t HTTP-Redirect sha512 exact 2
-t HTTP-POST sha256 minimum 2
-# t HTTP-Redirect sha512 maximum 2
-# t HTTP-POST sha256 better 2 # not working atm
-# t HTTP-Redirect sha512 exact 3
-# t HTTP-POST sha256 minimum 3
-t HTTP-Redirect sha512 maximum 3
+# Wait for all tests and collect exit codes
+FAIL=0
+for PID in "${PIDS[@]}"; do
+  wait $PID || FAIL=1
+done
+
+exit $FAIL
