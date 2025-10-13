2228f945fb4aec766b43ac09b129c770e5661342
Gianpiero Papappicco <68102511+gianpieropa@users.noreply.github.com>
2025-10-13 12:13:11 +0200

    Update idp-metadata.ts
    Remove console log

--- src/idp-metadata.ts
+++ src/idp-metadata.ts
@@ -30,8 +30,6 @@ export const getIdpCert = (idp: Element) => {
       });
     });
 
-    console.log('certificates', certificates);
-
     cert = certificates.find((certificate) => {
       // Find a not expired X509Certificate
       // Convert from base64 to pem format
@@ -39,28 +37,19 @@ export const getIdpCert = (idp: Element) => {
         .match(/.{1,64}/g)
         .join('\n')}\n-----END CERTIFICATE-----`;
 
-      console.log('pemCert', pemCert);
       try {
         const { validTo } = new X509Certificate(pemCert || '');
-        console.log('validTo', new Date(validTo));
-        console.log('new Date()', new Date());
         return new Date(validTo) > new Date();
       } catch (e) {
-        console.log('Error parsing certificate', e);
         return false;
       }
     });
-    console.log('cert', cert);
 
     if (!cert)
       cert = idp
         .getElementsByTagNameNS(NS.SIG, 'X509Certificate')
         .item(0)?.textContent;
   }
-  console.log(
-    'old cert',
-    idp.getElementsByTagNameNS(NS.SIG, 'X509Certificate').item(0)?.textContent,
-  );
   return cert;
 };
 
